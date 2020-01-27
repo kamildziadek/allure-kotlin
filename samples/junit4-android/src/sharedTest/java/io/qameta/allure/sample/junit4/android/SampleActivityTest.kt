a9353361582b1fe137ff1cb0491ae9a502552c32
Kamil Dziadek <dziadekk@ryanair.com>
2020-01-27 15:03:45 +0100

    Using allure kotlin in sample `junit4-android`
    * adding `AndroidManifest` for debug build type with required READ/WRITE external storage permissions
    * defining custom `allure.properties` files for unit tests and on device tests
      * defining allure report directory
      * specifying URL to issues
    * implementing tests
      * implementing basic and parametrized unit test
      * implementing sample activity test with the most important features of Allure

---
+++ samples/junit4-android/src/sharedTest/java/io/qameta/allure/sample/junit4/android/SampleActivityTest.kt
@@ -0,0 +1,119 @@
+package io.qameta.allure.sample.junit4.android
+
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import io.qameta.allure.Allure
+import io.qameta.allure.Allure.description
+import io.qameta.allure.Allure.feature
+import io.qameta.allure.Allure.issue
+import io.qameta.allure.Allure.step
+import io.qameta.allure.Allure.story
+import io.qameta.allure.Allure.tms
+import io.qameta.allure.Description
+import io.qameta.allure.Epic
+import io.qameta.allure.Feature
+import io.qameta.allure.Issue
+import io.qameta.allure.Lead
+import io.qameta.allure.Owner
+import io.qameta.allure.Severity
+import io.qameta.allure.SeverityLevel
+import io.qameta.allure.Story
+import io.qameta.allure.TmsLink
+import io.qameta.allure.android.AllureAndroidJUnit4
+import io.qameta.allure.android.extensions.screenshot
+import io.qameta.allure.android.rules.LogcatRule
+import io.qameta.allure.android.rules.ScreenshotRule
+import io.qameta.allure.junit4.DisplayName
+import io.qameta.allure.junit4.Tag
+import org.junit.*
+import org.junit.rules.*
+import org.junit.runner.*
+
+@RunWith(AllureAndroidJUnit4::class)
+@Epic("Samples")
+@DisplayName("SampleActivity tests")
+@Tag("Instrumentation")
+class SampleActivityTest {
+
+    @get:Rule
+    val ruleChain: RuleChain = RuleChain.outerRule(LogcatRule())
+        .around(ScreenshotRule(mode = ScreenshotRule.Mode.END, screenshotName = "screenshot_finish"))
+
+    @Before
+    fun setup() {
+        ActivityScenario.launch(SampleActivity::class.java)
+    }
+
+    @Test
+    @Feature("Successes")
+    @Story("Annotations")
+    @Issue("JIRA-123")
+    @TmsLink("TR-123")
+    @Owner("Some owner")
+    @Severity(value = SeverityLevel.BLOCKER)
+    @DisplayName("should text change after click with annotations")
+    @Description("Verification of text changing after interaction with a button")
+    fun shouldTextChangeAfterClickWithAnnotations() {
+        step("Click main button") {
+            Allure.screenshot("initial_state")
+            Espresso.onView(withId(R.id.button_some)).perform(click())
+        }
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        step("Verify text") {
+            Espresso.onView(withId(R.id.label_some))
+                .check(matches(withText(context.getString(R.string.after_click))))
+        }
+    }
+
+    @Test
+    @Lead("Some lead")
+    @Owner("Some owner")
+    @Severity(value = SeverityLevel.BLOCKER)
+    @DisplayName("should text change after click with methods")
+    fun shouldTextChangeAfterClickWithMethods() {
+        feature("Successes")
+        story("Methods")
+        issue(name = "JIRA-123", url = null)
+        tms(name = "TR-123", url = null)
+        description("Verification of text changing after interaction with a button")
+
+        step("Click main button") {
+            Allure.screenshot("initial_state")
+            Espresso.onView(withId(R.id.button_some)).perform(click())
+        }
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        step("Verify text") {
+            Espresso.onView(withId(R.id.label_some))
+                .check(matches(withText(context.getString(R.string.after_click))))
+        }
+    }
+
+    @Test
+    @Feature("Failures")
+    @Story("Initial state")
+    @Issue("JIRA-456")
+    @TmsLink("TR-456")
+    @Severity(value = SeverityLevel.CRITICAL)
+    @DisplayName("should initial state fail")
+    @Description("Verification of initial state that should fail on purpose (just so it is shown as a broken test by Allure)")
+    fun shouldInitialStateFail() {
+        step("Verify init text state") {
+            Espresso.onView(withId(R.id.label_some))
+                .check(matches(withText("FOO")))
+
+        }
+    }
+
+    @Test
+    @Ignore("Ignore example")
+    @Description("Empty test that ought to be ignored")
+    fun shouldIgnore() {
+    }
+
+}
---
+++ samples/junit4-android/src/test/java/io/qameta/allure/sample/junit4/android/SampleCalculatorTests.kt
@@ -0,0 +1,95 @@
+package io.qameta.allure.sample.junit4.android
+
+import io.qameta.allure.Allure.parameter
+import io.qameta.allure.Allure.step
+import io.qameta.allure.Description
+import io.qameta.allure.Epic
+import io.qameta.allure.Feature
+import io.qameta.allure.junit4.AllureParametrizedRunner
+import io.qameta.allure.junit4.AllureRunner
+import io.qameta.allure.junit4.DisplayName
+import io.qameta.allure.junit4.Tag
+import org.hamcrest.CoreMatchers.*
+import org.junit.*
+import org.junit.Assert.*
+import org.junit.runner.*
+import org.junit.runners.*
+
+@RunWith(AllureRunner::class)
+@Epic("Samples")
+@DisplayName("SampleCalculator simpletests")
+@Tag("Unit test")
+class SampleCalculatorTest {
+
+    @Test
+    @DisplayName("addition test")
+    @Feature("Addition")
+    @Description("Checks if addition is implemented correctly")
+    fun additionTest() {
+        val x = 2
+        val y = 4
+        parameter("x", x)
+        parameter("y", y)
+
+        val actual = step("Add values") {
+            SampleCalculator().add(x = x, y = y)
+        }
+        step("Verify correctness") {
+            assertThat(actual, `is`(6))
+        }
+
+    }
+
+    @Test
+    @Feature("Subtraction")
+    @DisplayName("subtraction test")
+    @Description("Checks if subtractions is implemented correctly")
+    fun subtractionTest() {
+        val x = 2
+        val y = 4
+        parameter("x", x)
+        parameter("y", y)
+
+        val actual = step("Subtract values") {
+            SampleCalculator().subtract(x = x, y = y)
+        }
+        step("Verify correctness") {
+            assertThat(actual, `is`(-2))
+        }
+
+    }
+}
+
+@RunWith(AllureParametrizedRunner::class)
+@Epic("Samples")
+@DisplayName("SampleCalculator Parameterized tests")
+@Tag("Unit test")
+class SampleCalculatorParametrizedTest(val x: Int, val y: Int, val expected: Int) {
+
+    @Test
+    @Feature("Addition")
+    @DisplayName("addition parametrized test")
+    fun additionTest() {
+        parameter("x", x)
+        parameter("y", y)
+
+        val actual = step("Add values") {
+            SampleCalculator().add(x = x, y = y)
+        }
+        step("Verify correctness") {
+            assertThat(actual, `is`(expected))
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun parameters() = arrayOf(
+            arrayOf(2, 2, 4),
+            arrayOf(2, 4, 6),
+            arrayOf(2, 10, 12)
+        )
+
+    }
+}
+
